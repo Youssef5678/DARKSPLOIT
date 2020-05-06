8350295e2e039d84730e29f0beee9f3fae62a906
Azertiox-Hacking <55140614+Youssef5678@users.noreply.github.com>
2020-05-06 15:02:42 +0000

    Add files via upload

---
+++ banner4.py
@@ -0,0 +1,10 @@
+# Set Color
+L = '\033[1;36m' #L
+print ""+L+""
+print " ____________"
+print "< Youssef_HR >"
+print " ------------"
+print "       \   ,__,"
+print "        \  (oo)____"
+print "           (__)    )\ "
+print "              ||--|| *"
